99a2d7ad531371bf5bc9b68b787789993597f80a
Edgar Snow <edgarsnow572000@gmail.com>
2023-04-13 01:16:50 +0630

    Added common folder

--- Jump-Start/src/App.jsx
+++ Jump-Start/src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
+import Header from "./Components/Common/Header";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
