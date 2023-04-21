e6aa5b6d5ffdd3a3be2111ebb0248011eb2cb092
hndrisan <hndrisantoso@yahoo.com>
2023-04-21 15:01:33 +0700

    i've added the contact

--- Jump-Start/src/App.jsx
+++ Jump-Start/src/App.jsx
@@ -8,7 +8,9 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import AboutUs from "./Components/AboutUs";
 import Privacy from "./Components/Privacy";
-import Return from "./Components/Return"
+import Return from "./Components/Return";
+import Contact from "./Components/Contact"
+import Map from "./Components/Location/Map";
 
 const App = () => {
   return (
@@ -21,6 +23,8 @@ const App = () => {
           <Route path="/About" element={<AboutUs/>}></Route>
           <Route path="/Privacy" element={<Privacy/>}></Route>
           <Route path="/Return" element={<Return/>}></Route>
+          <Route path="/Contact" element={<Contact/>}></Route>
+          <Route path="/Map" element={<Map/>}></Route>
         </Routes>
       </BrowserRouter>
 
---
+++ Jump-Start/src/Components/Location/location.jsx
@@ -0,0 +1,8 @@
+const location ={   
+    address : 'Van Boetzelaerstraat 49, 1051 CZ Amsterdam, Netherlands',
+    lat : 52.38229,
+    lng : 4.87206,
+
+}
+
+export default Amsterdam
