420cdca686ef1382b32086677b94aa2a75e97ece
Edgar Snow <edgarsnow572000@gmail.com>
2023-05-03 16:27:08 +0630

    Added Category filtering and storeName to items

---
+++ Jump-Start/src/Components/Products/Category/FullLists/GiftFullList.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import SingleProduct from "../../SingleProduct";
+import StoreService from "../../../../services/StoreService";
+
+const GiftFullList = (props) => {
+  const [items, setItems] = useState([]);
+  const [visibleItems, setVisibleItems] = useState([]);
+
+  const viewItems = async () => {
+    try {
+      const items = await StoreService.getItems();
+
+      // Fetch the store name for each item
+      const updatedItems = await Promise.all(
+        items.map(async (item) => {
+          const store = await StoreService.getStore(item.storeID);
+          return { ...item, storeName: store.storeName };
+        })
+      );
+
+      setItems(updatedItems);
+      console.log(updatedItems);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    viewItems();
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const bottom =
+        Math.ceil(window.innerHeight + window.scrollY) >=
+        document.documentElement.scrollHeight;
+
+      if (bottom) {
+        const currentLength = visibleItems.length;
+        const nextIndex = currentLength + 10;
+        const nextItems = items.slice(currentLength, nextIndex);
+        setVisibleItems([...visibleItems, ...nextItems]);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [items, visibleItems]);
+
+  return (
+    <div>
+      <div className="mb-6 mt-8">
+        <div>
+          <h2 className="font-bold text-4xl text-center">Gift Products</h2>
+        </div>
+        <div className="grid grid-cols-4 gap-y-10 mt-5 ml-12">
+          {visibleItems.length > 0 ? (
+            visibleItems
+              .filter((item) => item.itemCategory === "Gift")
+              .map((item) => <SingleProduct key={item._id} item={item} />)
+          ) : (
+            <div>
+              <h3 className="text-lg text-center">
+                There are no items to display. Sorry for the inconvience!
+              </h3>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GiftFullList;
